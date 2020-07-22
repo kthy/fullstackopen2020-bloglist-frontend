ed93b1682261a7446b3ee6409ae089dcc8bb8e32
Kristian Thy <thy@42.dk>
2020-07-22 23:29:34 +0200

    5.11: Blog list frontend, step11

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Button = ({whenClicked, label}) => <button onClick={whenClicked}>{label}</button>
+const Button = ({ whenClicked, label }) => <button onClick={whenClicked}>{label}</button>
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  whenClicked: PropTypes.func.isRequired,
+}
 
 export default Button
